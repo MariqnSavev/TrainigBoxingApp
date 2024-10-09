e103c52a7d22357e153c0729584dcc5d9e5a5a4c
eon <dobrin_k1@abv.bg>
2024-10-09 09:53:16 +0300

    Updates

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.NetworkInformation;
+using TrainigBoxingApp.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+namespace TrainigBoxingApp.Data
+
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Workout> Workouts { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace TrainigBoxingApp.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public ICollection<Workout> Workouts { get; set; }
+    }
+}
